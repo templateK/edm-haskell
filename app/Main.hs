4f37740b43e046abcd7b1c410f809f6dd47a0487
templateK <templateK@github.com>
2018-08-31 22:28:14 +0900

    proper subdirectory detection

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,39 @@ module Main where
 import Control.Lens
 import Distribution.PackageDescription
 import Distribution.PackageDescription.Parsec
+import Distribution.Simple.Utils
 import Distribution.Verbosity
 import Distribution.Types.UnqualComponentName
 import qualified Distribution.Types.Lens    as L
 import Distribution.Types.PackageName
 import Data.Maybe
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C8
 
+import System.Directory
+import System.FilePath
+import Data.List ((\\), sortBy)
+import Data.Ord (Down(..), comparing)
 
 main :: IO ()
 main = do
-  let filename = "sample_cabal.txt"
-  foo <- getCabalTarget filename "src"
-  print foo
+  cur <- getCurrentDirectory
+  let parent  = takeDirectory cur
+      curPath = takeFileName cur
+  print  "ff"
 
 
 -- NOTE: Don't deal with specific file. Only depend on path.
 --       You may be have to deal with file searching on
 --       current directory if you want to check the filename.
-getCabalTarget :: FilePath -> FilePath -> IO (Maybe String)
-getCabalTarget cabalFilePath path = do
+-- getCabalTarget :: FilePath -> FilePath -> IO String
+getCabalTarget cabalFilePath pwd = do
+
+  -- pwd <- getCurrentDirectory
+  let prjRoot = dropFileName cabalFilePath
+      relPath = joinPath $ splitPath pwd \\ splitPath prjRoot
+  -- print $ relPath
+
 
   genPkgsDesc <- readGenericPackageDescription normal cabalFilePath
 
@@ -39,18 +53,37 @@ getCabalTarget cabalFilePath path = do
                          . _2
                          . to condTreeData
                          . exesLens
+  -- print $ libs
+  -- print $ exes
   -- NOTE: For now, if we fail to find proper component name on current path,
-  --       We use exe:<global package name> as the default the value of the cabal target.
+  --       We use empty string as the default the value of the cabal target.
   --       Or? Just return emtpy string?? or... Maybe value
-  return $ if path `isSubOf` libs
-             then Just $ "lib:" <> gpkg
-             else (<>) "exe:" <$> (path `compOf` exes)
+  return $ if relPath `isSubOf` libs
+             then "lib:" <> gpkg
+             else fromMaybe "" $ ((<>) "exe:" . fst) <$> (relPath `compOf` exes)
   where
-    -- TODO: proper subdirectory testing
-    isSubOf p dirs = fromMaybe False (elemOf folded p <$> dirs)
-    compOf p dirs  = fst <$> findOf folded (elem p . snd) dirs
+    hasChildIn p   = anyOf folded (p `isParentDirOf`)
+    isSubOf p dirs = fromMaybe False ((p `hasChildIn`) <$> dirs)
+    compOf p dirs  = firstOf traverse
+                      $ sortLongest
+                      $ dirs ^.. folded . filtered ((p `hasChildIn`) . snd)
     gpkgLens = L.pkgName . to unPackageName
-    libsLens = L.hsSourceDirs
+    libsLens = L.hsSourceDirs . to (fmap normalise)
     exesLens = runGetter $ (,)
                 <$> Getter (L.exeName . to unUnqualComponentName)
-                <*> Getter L.hsSourceDirs
+                <*> Getter (L.hsSourceDirs . to (fmap normalise))
+
+
+isParentDirOf :: FilePath -> FilePath -> Bool
+isParentDirOf parent child = length child' == (length $ takeWhile id ee)
+  where
+    parent' = splitDirectories parent
+    child' = splitDirectories child
+    ee = zipWith (==) parent' child'
+
+
+-- Longest path is the closest path to the target path.
+-- ex) The closest path to "app/foo/bar/wat" is "app/foo/bar"
+--     among "app", "app/foo" and "app/foo/bar".
+sortLongest :: [(a, [FilePath])] -> [(a, [FilePath])]
+sortLongest = sortBy (comparing $ Down . maximum . fmap length . snd)
