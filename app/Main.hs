13ada31ce7664fee7c56f9f892a455ad23d1ebfa
templateK <templateK@github.com>
2018-09-03 05:47:19 +0900

    more fine grained exe component target searching

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ module Main where
 
 
 import Control.Lens
-import Distribution.PackageDescription
+import Distribution.PackageDescription hiding (exeName)
 import Distribution.PackageDescription.Parsec
 import Distribution.PackageDescription.PrettyPrint
 import Distribution.Verbosity
@@ -20,11 +20,19 @@ import Data.Ord (Down(..), comparing)
 import Data.Foldable (asum)
 
 
+data ExeComp = ExeComp
+  { exeName :: String
+  , mainIs  :: String
+  , srcDirs :: [FilePath]
+  } deriving (Show)
+
+
+
 main :: IO ()
 main = do
-  print =<< getCabalTarget "applied-fp-course.txt" "src"
+  -- print =<< getCabalTarget "applied-fp-course.txt" "src"
+  -- print =<< getCabalTarget "applied-fp-course.txt" "tests"
   print =<< getCabalTarget "applied-fp-course.txt" "tests"
-  print =<< getCabalTarget "applied-fp-course.txt" "exe"
   -- print =<< ("flib:fcomp"==) <$> getCabalTarget "foreign-library-cabal.txt" "lib"
   -- print =<< ("flib:fcomp"==) <$> getCabalTarget "foreign-library-cabal.txt" "lib/foo/bar"
   -- print =<< ("exe:top"   ==) <$> getCabalTarget "sample_cabal.txt" "app"
@@ -46,51 +54,45 @@ getCabalTarget cabalFilePath pwd = do
 
   let gpkg = genPkgsDesc ^. L.packageDescription . to package . gpkgLens
       libs = genPkgsDesc ^. L.condLibrary ^? _Just . to condTreeData . libsLens
-      sibs = genPkgsDesc ^. L.condSubLibraries ^.. sibsLens
-      fibs = genPkgsDesc ^. L.condForeignLibs  ^.. fibsLens
-      exes = genPkgsDesc ^. L.condExecutables  ^.. exesLens
-      tsts = genPkgsDesc ^. L.condTestSuites   ^.. tstsLens
-      bchs = genPkgsDesc ^. L.condBenchmarks   ^.. bchsLens
+      exes = genPkgsDesc ^. L.condExecutables  ^.. folded . _2 . to condTreeData . exesLens
+      -- sibs = genPkgsDesc ^. L.condSubLibraries ^.. sibsLens
+      -- fibs = genPkgsDesc ^. L.condForeignLibs  ^.. fibsLens
   -- NOTE: For now, if we fail to find proper component name on current path,
   --       We use empty string as the default the value of the cabal target.
   --       Or? Just return emtpy string?? or... Maybe value
   let defaultValue = ""
       match = [ if relPath `isAnySubdirOf` libs then Just ("lib:" <> gpkg) else Nothing
-              , mkCabalTarget relPath "lib:"   sibs -- sub library qualfication is `lib`
-              , mkCabalTarget relPath "exe:"   exes
-              , mkCabalTarget relPath "flib:"  fibs
-              , mkCabalTarget relPath "test:"  tsts
-              , mkCabalTarget relPath "bench:" bchs ]
+              , mkExeTarget relPath "exe:"   exes ] 
+              -- , mkCabalTarget relPath "lib:"   sibs -- sub library qualfication is `lib`
+              -- , mkCabalTarget relPath "flib:"  fibs ]
   -- NOTE: Maybe is instance of Alternative. So asum returns the first Just value.
   return $ fromMaybe defaultValue (asum match)
   where
     gpkgLens = L.pkgName . to unPackageName
     libsLens = L.hsSourceDirs . to (fmap normalise)
-    exesLens = compLens $ Getter (L.exeName          . to unUnqualComponentName)
-    fibsLens = compLens $ Getter (L.foreignLibName   . to unUnqualComponentName)
-    sibsLens = compLens $ Getter (L.libName . non "" . to unUnqualComponentName)
-    tstsLens = compLens $ Getter (L.testName         . to unUnqualComponentName)
-    bchsLens = compLens $ Getter (L.benchmarkName    . to unUnqualComponentName)
-    compLens gttr =
-      folded . _2 . to condTreeData
-             . runGetter ((,) <$> gttr <*> Getter (L.hsSourceDirs . to (fmap normalise)))
-
+    exesLens = runGetter (ExeComp <$> Getter (L.exeName      . to unUnqualComponentName)
+                                  <*> Getter (L.modulePath   . to normalise            )
+                                  <*> Getter (L.hsSourceDirs . to (fmap normalise))    )
+    -- compLens gttr =
+    --   folded . _2 . to condTreeData
+    --          . runGetter ((,) <$> gttr <*> Getter (L.hsSourceDirs . to (fmap normalise)))
 
 isAnySubdirOf :: FilePath -> Maybe [FilePath] -> Bool
 isAnySubdirOf p dirs = fromMaybe False ((p `hasChildIn`) <$> dirs)
 
 
-mkCabalTarget :: Semigroup b => FilePath -> b -> [(b, [FilePath])] -> Maybe b
-mkCabalTarget path prefix dirs = ((<>) prefix . fst) <$> (path `compOf` dirs)
+mkExeTarget :: FilePath -> String -> [ExeComp] -> Maybe String
+mkExeTarget path prefix comps = ((<>) prefix . exeName) <$> (path `sameOrClosest` comps)
   where
-    compOf :: FilePath -> [(a, [FilePath])] -> Maybe (a, [FilePath])
-    compOf p dirs  = firstOf (traverse . filtered ((p `hasChildIn`) . snd))
-                       $ sortLongest dirs
+    sameOrClosest p cs = asum (path `samePathOf` comps , path `closestOf` cs)
+    closestOf  p cs    = firstOf traverse (p `subDirOf` cs) 
+    samePathOf p cs    = findOf traverse  (\x ->  p ==  mainIs x) (p `subDirOf` cs) 
+    subDirOf p cs      = toListOf (traverse . filtered ((p `hasChildIn`) . srcDirs)) (sortLongest cs) 
     -- NOTE: Longest path is the closest path to the target path.
     -- ex) The closest path to "app/mkCabalTarget/bar/wat" is "app/mkCabalTarget/bar"
     --     among "app", "app/mkCabalTarget" and "app/mkCabalTarget/bar".
-    sortLongest :: [(a, [FilePath])] -> [(a, [FilePath])]
-    sortLongest = sortBy (comparing $ Down . maximum . fmap length . snd)
+    sortLongest :: [ExeComp] -> [ExeComp]
+    sortLongest = sortBy (comparing $ Down . maximum . fmap length . srcDirs)
 
 
 hasChildIn :: FilePath -> [FilePath] -> Bool
